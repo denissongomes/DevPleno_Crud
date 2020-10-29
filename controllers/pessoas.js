4abd6841ff0cf3dbba569fae5acef3892e9d8ce9
Denisson Gomes <dnsgom@gmail.com>
2020-10-29 14:09:58 -0300

    Add deleteRow controller

--- controllers/pessoas.js
+++ controllers/pessoas.js
@@ -5,6 +5,15 @@ const index = async(connection, req, res) => {
   res.render('pessoas/index', { pessoas: results })
 }
 
+const deleteRow = async(connection, req, res) => {
+  //deleta registro
+  await pessoas.deleteRow(connection, req.params.id)
+ //e redireciona novamente para 'pessoas'
+  res.redirect('/pessoas')
+}
+
+
 module.exports = {
-    index
+    index,
+    deleteRow
 }
