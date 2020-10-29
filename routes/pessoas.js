b2b4e48412c753b6b17036ad731c0b868ba4b660
Denisson Gomes <dnsgom@gmail.com>
2020-10-29 08:34:48 -0300

    Transfer function to controller pessoas

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -2,11 +2,7 @@ const express = require('express')
 
 const pessoasRouter = ({connection}) => {
     const router = express.Router()
-    router.get('/', (req, res) => {
-        connection.query('select * from pessoas', (err, results) => {
-            res.send(results)
-        })  
-    })
+    router.get('/', )
     return router
 }
 
