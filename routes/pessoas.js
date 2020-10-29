d79222fb329e74a12b7f3832658816733660d54b
Denisson Gomes <dnsgom@gmail.com>
2020-10-29 07:37:13 -0300

    Inject dependencies object to function

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const pessoasRouter = () => {
+const pessoasRouter = (dependencies) => {
     const router = express.Router()
     router.get('/', (req, res) => {
         res.send('Pessoas')
