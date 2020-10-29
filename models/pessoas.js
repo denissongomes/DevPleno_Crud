bdc24db06d3fef864e1947c5cb4d1320c7593cc1
Denisson Gomes <dnsgom@gmail.com>
2020-10-29 12:20:45 -0300

    Create a function to delete rows

--- models/pessoas.js
+++ models/pessoas.js
@@ -1,6 +1,6 @@
 const findAll = (connection) => {
     return new Promise((resolve, reject) => {
-        connection.query('select * from pessoas', (err, results) => {
+        connection.query('SELECT * FROM pessoas', (err, results) => {
             if(err){
                 reject(err)
             } else {
@@ -11,6 +11,18 @@ const findAll = (connection) => {
     }) 
 }
 
+const deleteOne = (connection) => {
+    return new Promise((resolve,reject) => {
+        connection.query('DELETE FROM pessoas WHERE id='+id), (err, results) => {
+            if(err){
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        }
+    })
+}
+
 module.exports = {
-    findAll
+    findAll, deleteOne
 }
